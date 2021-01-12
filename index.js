e04f650ab878a5ec1e4f078da373fa5c8d78b060
Zach <zbayard@gmail.com>
2021-01-12 13:01:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.querySelector('#main').remove()
+
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+
+newHeader.textContent = "Zach is the champion"
